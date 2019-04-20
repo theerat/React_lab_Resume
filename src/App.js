3eb87851c06fae39c4f077f66bc90a074db3a7c8
Dev_Theerat <theerat56@outlook.com>
2019-04-20 14:48:28 +0700

    Update25620420
    Fix layout responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Rate, Card, Timeline } from 'antd';
+import { Rate, Card, Timeline, Row, Col } from 'antd';
 import './App.css';
 
 class App extends Component {
@@ -8,46 +8,49 @@ class App extends Component {
 
     const { Meta } = Card;
     return (
-      <div className="App ant-card ant-card-bordered" style={{ padding: 50, opacity: 0.8 }}>
-        <div className="app-left">
-          <Card
-            hoverable
-            style={{ width: 300 }}
-            cover={<img alt="profile" className="pro-Images" src={require("./Images/myPic.jpg")} />}
-          > <Meta
-              title="Theerat Kanisaraphongphon"
-              description="Software Developer"
-            />
-          </Card>
-          <div style={{ marginTop: 50 }}>
+      <div className="App ant-card ant-card-bordered" style={{}}>
+        <Row>
+          <Col lg={12} md={12}>
+            <Card
+              hoverable
+              style={{ width: 300 }}
+              cover={<img alt="profile" className="pro-Images" src={require("./Images/myPic.jpg")} />}
+            > <Meta
+                title="Theerat Kanisaraphongphon"
+                description="Software Developer"
+              />
+            </Card>
+            <div style={{ marginTop: 50 }}>
+              <div>
+                <h2>Education</h2>
+                <ul>
+                  <li><b>RMUTK</b></li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+          <Col lg={12} md={12}>
             <div>
-              <h2>Education</h2>
+              <h2>Experience</h2>
+              <Timeline className="elTimeline">
+                <Timeline.Item><b>2558-2560 - TECHCONSBIZ CO, LTD</b></Timeline.Item>
+                <Timeline.Item><b>2560-2561 - LH BANK PUB CO, LTD</b></Timeline.Item>
+                <Timeline.Item><b>PRESENT - OUTSOURCE (PTT DIGITAL)</b></Timeline.Item>
+              </Timeline>,
+          </div>
+            <div style={{ marginTop: 20 }}>
+              <h2>Skill</h2>
               <ul>
-                <li><b>RMUTK</b></li>
+                <li ><Rate allowHalf defaultValue={4} /><b>C#</b></li>
+                <li><Rate allowHalf defaultValue={3} /><b>API</b></li>
+                <li><Rate allowHalf defaultValue={3} /><b>.NET CORE</b></li>
+                <li><Rate allowHalf defaultValue={3} /><b>SHAREPOINT</b></li>
+                <li><Rate allowHalf defaultValue={2} /><b>JQUERY</b></li>
               </ul>
             </div>
-          </div>
-        </div>
-        <div className="app-right">
-          <div >
-            <h2>Experience</h2>
-            <Timeline>
-              <Timeline.Item><b>2558-2560 - TECHCONSBIZ CO, LTD</b></Timeline.Item>
-              <Timeline.Item><b>2560-2561 - LH BANK PUB CO, LTD</b></Timeline.Item>
-              <Timeline.Item><b>PRESENT - OUTSOURCE (PTT DIGITAL)</b></Timeline.Item>
-            </Timeline>,
-          </div>
-          <div style={{ marginTop: 20 }}>
-            <h2>Skill</h2>
-            <ul>
-              <li ><Rate allowHalf defaultValue={4} /><b>C#</b></li>
-              <li><Rate allowHalf defaultValue={3} /><b>API</b></li>
-              <li><Rate allowHalf defaultValue={3} /><b>.NET CORE</b></li>
-              <li><Rate allowHalf defaultValue={3} /><b>SHAREPOINT</b></li>
-              <li><Rate allowHalf defaultValue={2} /><b>JQUERY</b></li>
-            </ul>
-          </div>
-        </div>
+          </Col>
+
+        </Row>
       </div>
     );
   }
