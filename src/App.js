24fcd0f3ab0cc7c30609a0ba1d2a8a13ae015542
Dev_Theerat <theerat56@outlook.com>
2019-04-20 15:09:52 +0700

    Update Connect me

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Rate, Card, Timeline, Row, Col } from 'antd';
+import { Rate, Card, Timeline, Row, Col, Divider, Icon } from 'antd';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +13,7 @@ class App extends Component {
           <Col lg={12} md={12}>
             <Card
               hoverable
-              style={{ width: 300 }}
+              className="card"
               cover={<img alt="profile" className="pro-Images" src={require("./Images/myPic.jpg")} />}
             > <Meta
                 title="Theerat Kanisaraphongphon"
@@ -22,16 +22,16 @@ class App extends Component {
             </Card>
             <div style={{ marginTop: 50 }}>
               <div>
-                <h2>Education</h2>
+                <Divider orientation="left">EDUCATION</Divider>
                 <ul>
-                  <li><b>RMUTK</b></li>
+                  <li><b>Rajamangala University of Technology Krungthep (RMUTK)</b></li>
                 </ul>
               </div>
             </div>
           </Col>
           <Col lg={12} md={12}>
             <div>
-              <h2>Experience</h2>
+              <Divider orientation="left">EXPERIENCE</Divider>
               <Timeline className="elTimeline">
                 <Timeline.Item><b>2558-2560 - TECHCONSBIZ CO, LTD</b></Timeline.Item>
                 <Timeline.Item><b>2560-2561 - LH BANK PUB CO, LTD</b></Timeline.Item>
@@ -39,13 +39,20 @@ class App extends Component {
               </Timeline>,
           </div>
             <div style={{ marginTop: 20 }}>
-              <h2>Skill</h2>
+              <Divider orientation="left">SKILL</Divider>
               <ul>
                 <li ><Rate allowHalf defaultValue={4} /><b>C#</b></li>
                 <li><Rate allowHalf defaultValue={3} /><b>API</b></li>
                 <li><Rate allowHalf defaultValue={3} /><b>.NET CORE</b></li>
                 <li><Rate allowHalf defaultValue={3} /><b>SHAREPOINT</b></li>
                 <li><Rate allowHalf defaultValue={2} /><b>JQUERY</b></li>
+              </ul>
+            </div>
+            <div style={{ marginTop: 20 }}>
+              <Divider orientation="left">CONNECT ME AT</Divider>
+              <ul>
+                <li><Icon type="facebook" style={{ fontSize: '30px', paddingRight: '10px', paddingTop: '10px' }} /><b><a href="https://www.facebook.com/julong.boybas">Theerat Kanisaraphongphon</a></b></li>
+                <li><Icon type="github" style={{ fontSize: '30px', paddingRight: '10px', paddingTop: '10px' }} /><b><a href="https://github.com/theerat">https://github.com/theerat</a></b></li>
               </ul>
             </div>
           </Col>
